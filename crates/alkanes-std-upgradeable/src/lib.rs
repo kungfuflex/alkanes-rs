0bfe2e3af2778d73878adbdc414544ffbfddf674
Ubuntu <ubuntu@melee-mardi-gras.us-central1-a.c.oyl-project-123456.internal>
2025-07-18 21:00:33 +0000

    update

--- crates/alkanes-std-upgradeable/src/lib.rs
+++ crates/alkanes-std-upgradeable/src/lib.rs
@@ -1,15 +1,13 @@
 use alkanes_runtime::auth::AuthenticatedResponder;
 use alkanes_runtime::declare_alkane;
 use alkanes_runtime::message::MessageDispatch;
-#[allow(unused_imports)]
-use alkanes_runtime::{
-    println,
-    stdio::{stdout, Write},
+use {
+  alkanes_runtime::{println, stdio::{stdout}},
+  std::fmt::Write
 };
 use alkanes_runtime::{runtime::AlkaneResponder, storage::StoragePointer};
 use alkanes_support::{cellpack::Cellpack, id::AlkaneId, response::CallResponse};
 use anyhow::{anyhow, Result};
-use metashrew_support::compat::{to_arraybuffer_layout, to_passback_ptr};
 use metashrew_support::index_pointer::KeyValuePointer;
 use std::sync::Arc;
 
