a7ee14a2407922817cdfb6b642d28313b334d842
kungfuflex <judoflexchop@gmail.com>
2025-11-07 12:59:07 -0500

    update

---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+//! alkanes-rs workspace root library
+//!
+//! This is the root library for the alkanes-rs workspace.
+//! The main alkanes indexer implementation is in the `alkanes` crate.
+
+pub use alkanes;
