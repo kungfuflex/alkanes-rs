3ad2bc1de57a34dccb4d92a98684177aeac05940
kungfuflex <judoflexchop@gmail.com>
2025-12-23 22:09:33 +0000

    rm backup

--- backup_20251223_211911/alkanes/src/precompiled/alkanes_std_genesis_alkane_upgraded_eoa_mainnet_build.rs
+++
@@ -1,3 +0,0 @@
-pub fn get_bytes() -> Vec<u8> {
-    include_bytes!("./alkanes_std_genesis_alkane_upgraded_eoa_mainnet.wasm").to_vec()
-}
--- backup_20251223_211911/alkanes/src/tests/networks.rs
+++
@@ -1,7 +0,0 @@
-#[cfg(feature = "regtest")]
-mod networks {
-    use wasm_bindgen_test::*;
-
-    #[wasm_bindgen_test]
-    fn test_protorunes_view_by_address() {}
-}
